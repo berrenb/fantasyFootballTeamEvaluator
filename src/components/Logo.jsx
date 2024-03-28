2476de6f1957e0e1ebf22fb4cef6ee43d66a5949
Bryan Berrent <bryan.berrent@gmail.com>
2024-03-27 20:14:31 -0400

    Updates

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/App.css";
-import logoSrc from "../resources/images/logo.png";
-
-export default function Logo({ className, style}) {
-    return (
-        <img src={logoSrc} alt="Draft Logo" className={className} style={style} />
-    );
-}
